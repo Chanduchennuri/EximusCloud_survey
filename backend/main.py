7700872c47cf57340130b39e9524ca69cc7b4b34
Austincollab <austingriffin72093@gmail.com>
2026-08-18 11:40:23 +0530

    netlify url configuration cors

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://eximuscloudpulse.netlify.app/",
+        "https://eximuscloudpulse.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
